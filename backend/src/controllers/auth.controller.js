1b46f62a206393ce8971f6f5fb6bdeaecddf3c87
Abhishek Pawar <apawar0078@gmail.com>
2025-06-13 15:58:58 +0530

    auth api and message api backend changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -81,5 +82,39 @@ export const login = async (req,res) => {
 };
 
 export const logout = (req,res) => {
-    res.send("logout route");
+    try {
+        res.cookie("jwt","",{maxAge: 0});
+        res.status(200).json({message: "Logged Out Sucessfully"});
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
+
+export const updateProfile = async (req,res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic)
+            return res.status(500).json({message: "Profile Pic is required"});
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        const updateUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+
+        res.status(200).json(updateUser);
+
+    } catch (error) {
+        
+    }
+};
+
+export const checkAuth = (req,res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({message: "Internal Server Error"})
+    }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,6 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
-import { login } from "../controllers/auth.controller.js";
-import { logout } from "../controllers/auth.controller.js";
+import { signup, updateProfile,login,logout,checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -10,5 +9,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile",protectRoute,updateProfile);
+
+router.get("/check",protectRoute,checkAuth);
 
 export default router;
