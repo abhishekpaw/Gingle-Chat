5fb3bcf2f5666405eed7a9d0c5ebd92d32676255
Abhishek Pawar <apawar0078@gmail.com>
2025-08-24 21:42:52 +0530

    chatcontainer fix

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -44,10 +44,10 @@ const ChatContainer = () => {
 
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
-          <div key={message._id} className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`} ref={messageEndRef}>
+          <div key={message._id} className={`chat ${message.receiverId === authUser._id ? "chat-end" : "chat-start"}`} ref={messageEndRef}>
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
-                <img src={message.senderId === authUser._id ? authUser.profilePic || "/avatar.png" : selectedUser.profilePic || "/avatar.png"}
+                <img src={message.receiverId === authUser._id ? authUser.profilePic || "/avatar.png" : selectedUser.profilePic || "/avatar.png"}
                       alt="profile pic"/>
               </div>
             </div>
