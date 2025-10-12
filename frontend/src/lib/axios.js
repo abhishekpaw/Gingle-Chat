0a3c5d288b80ecdaff39bd4938f0eccf076e0806
Abhishek Pawar <apawar0078@gmail.com>
2025-10-12 21:44:19 +0530

    some fixe

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,5 @@
 
  export const axiosInstance = axios.create({
     baseURL: "https://gingle-chat.onrender.com/api",
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODZlYzIyNjFiMDdiY2Y1ODkxYmZmYjciLCJpYXQiOjE3NTYwNjI0MTcsImV4cCI6MTc1NjY2NzIxN30.kXjYw9VhuCiao5gQfJg_oOrj65TdnQqarwJ8V7JZAvo`
-    },
     withCredentials: true,
  });
